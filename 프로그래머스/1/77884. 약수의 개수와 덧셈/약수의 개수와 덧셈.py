6d799d647b64807d569e50124db7581629505f93
SeungHyun Kim <86511086+mooonu@users.noreply.github.com>
2025-08-11 17:26:17 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.31 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -2,18 +2,11 @@ def solution(left, right):
     # 약수의 개수가 짝수면 더하고
     # 홀수면 빼고
     
-    
     result = 0
     for i in range(left, right+1):
-        L = []
-        # 순회하며 약수의 개수를 계산하는 코드
-        for j in range(1, i+1):
-            if i % j == 0:
-                L.append(j)
-        # 길이가 짝수면 더하고 아니면 빼고
-        if len(L) % 2 == 0:
-            result += i
-        else:
+        if int(i**0.5) == i**0.5:
             result -= i
-    
+        else:
+            result += i
+            
     return result
